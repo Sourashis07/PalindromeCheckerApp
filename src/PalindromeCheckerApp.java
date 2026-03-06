7ae3b0e3166c8b919e18b8369495f30009c7be70
Sourashis07 <sourashis7ps7sabud@gmail.com>
2026-03-06 14:08:36 +0530

    Initial commit with Java file

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,44 @@
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase1PalindromeApp
+ * ============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ *
+ * The goal is to establish a clear startup flow.
+ *
+ * @author Oreoz
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME TO PALINDROME CHECKER APP MANAGEMENT SYSTEM");
+        System.out.println("version:3.0");
+        System.out.println("System instanced successful");
+        System.out.println();
+
+        String Palindrome = "racecar";
+        String Reverse = "";
+
+        int i;
+
+        for(i = Palindrome.length()-1; i >= 0; i--){
+            Reverse += Palindrome.charAt(i);
+        }
+        if (Palindrome.equals(Reverse))  System.out.println("The string " + Palindrome + " is a palindrome.");
+        else System.out.println("The string " + Palindrome + " is not a palindrome.");
+    }
+}
